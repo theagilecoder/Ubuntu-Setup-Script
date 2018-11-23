8fb52cdc51f3f3018e27650f4fbcaed99889c5e9
Pushkar Singh <theagilecoder@gmail.com>
2018-11-23 14:38:05 +0530

    Install Node

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,3 +6,21 @@ echo Starting Setup...
 # Install curl
 sudo apt install curl
 
+# Install git
+sudo apt install git
+git --version
+git config --global user.name "Pushkar Singh"
+git config --global user.email "theagilecoder@gmail.com"
+
+# Install NVM
+# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-18-04#installing-using-nvm
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh
+bash install_nvm.sh
+source ~/.profile
+nvm --version
+
+# Install Node
+nvm install node
+node --version
+npm --version
+
