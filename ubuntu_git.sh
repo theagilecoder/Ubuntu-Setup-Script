867f3008f3e34da4e8d2da4370b5cfb38af4ebc6
Pushkar Singh <theagilecoder@gmail.com>
2018-11-24 17:45:33 +0530

    Update ubuntu_git.sh

--- ubuntu_git.sh
+++ ubuntu_git.sh
@@ -2,7 +2,7 @@
 
 # Install git
 echo -e "\e[1;32m Installing git... \e[0m"
-sudo apt install git xlip
+sudo apt install git xclip
 git --version
 git config --global user.name "Pushkar Singh"
 git config --global user.email "theagilecoder@gmail.com"
