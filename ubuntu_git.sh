359bcd0117eeac15f4366e1b8da5e53c9c3cc842
Pushkar Singh <theagilecoder@gmail.com>
2018-11-24 17:37:00 +0530

    Update ubuntu_git.sh

--- ubuntu_git.sh
+++ ubuntu_git.sh
@@ -2,8 +2,15 @@
 
 # Install git
 echo -e "\e[1;32m Installing git... \e[0m"
-sudo apt install git
+sudo apt install git xlip
 git --version
 git config --global user.name "Pushkar Singh"
 git config --global user.email "theagilecoder@gmail.com"
 echo -e "\e[1;32m Git Setup complete... \e[0m"
+
+ssh-keygen -t RSA -b 4096 -C "theagilecoder@gmail.com"
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+echo -e "\e[1;32m Public Key copied... \e[0m"
+echo -e "\e[1;32m Now paste in Github... \e[0m"
