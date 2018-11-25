6359c10f1cde1b46a593fe2eb20c6e68ea00054a
Pushkar Singh <theagilecoder@gmail.com>
2018-11-25 11:12:57 +0530

    Update ubuntu_rails.sh

--- ubuntu_rails.sh
+++ ubuntu_rails.sh
@@ -6,3 +6,7 @@ gem install bundler
 gem install rails -v 5.2.1
 rails -v
 echo -e "\e[1;32m Rails Setup complete... \e[0m"
+
+# sqlite3 should be installed
+sudo apt install libsqlite3-dev sqlite3
+echo -e "\e[1;32m Gems Setup complete... \e[0m"
