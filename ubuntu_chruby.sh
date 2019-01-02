f8301cc0622c0b6dd372b7b3d17ca3c6c93de43c
Pushkar Singh <theagilecoder@gmail.com>
2019-01-02 14:34:17 +0530

    Update ubuntu_chruby.sh

--- ubuntu_chruby.sh
+++ ubuntu_chruby.sh
@@ -5,6 +5,7 @@
 # Install build essential if not installed
 # sudo apt install build-essential
 
+cd ~
 wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
 tar -xzvf ruby-install-0.7.0.tar.gz
 cd ruby-install-0.7.0/
